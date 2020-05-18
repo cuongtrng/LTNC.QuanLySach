7349ee01d64ad227f415a16a72f9a8f25101c9b8
PDLcute <65476800+PDLcute@users.noreply.github.com>
2020-05-18 11:02:40 +0700

    18/5/2020
    thêm add, edit,delete book (chuẩn)

--- QuanLySach_LTNC/src/quanlysach_ltnc/QuanLySach_LTNC.java
+++ QuanLySach_LTNC/src/quanlysach_ltnc/QuanLySach_LTNC.java
@@ -1,28 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package quanlysach_ltnc;
 
-import controller.AddBook;
+import view.AddBook;
 import view.LoginOption;
 
-/**
- *
- * @author Cuong
- */
 public class QuanLySach_LTNC {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-       // LoginOption lo=new LoginOption();
-       //lo.show();
-        AddBook ab= new AddBook();
-        ab.show();
-                
+        LoginOption.getInstance().setVisible(true);
+        
+        
     }
-    
 }
